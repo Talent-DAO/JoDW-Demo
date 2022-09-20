1da17af8580cd4db7f9ef3eb9ddbc27f42dcb6d6
Jaxcoder <codenamejason@outlook.com>
2022-09-20 18:09:45 -0400

    update default chains

--- packages/react-app/src/utils/wagmi.js
+++ packages/react-app/src/utils/wagmi.js
@@ -8,19 +8,23 @@ import { publicProvider } from "wagmi/providers/public";
 const alchemyId = process.env.ALCHEMY_ID;
 const infuraId = process.env.INFURA_ID;
 
-export const { chains, provider, webSocketProvider } = configureChains(
-  [chain.mainnet, chain.goerli, chain.polygonMumbai, chain.polygon, chain.optimism],
-  [
-    jsonRpcProvider({
-      rpc: chain => ({
-        http: "http://localhost:8545",
-      }),
+const activeChains = [];
+if (process.env.REACT_APP_ENV === "production") {
+  activeChains.push(chain.polygon);
+} else {
+  activeChains.push(chain.polygonMumbai);
+}
+
+export const { chains, provider, webSocketProvider } = configureChains(activeChains, [
+  jsonRpcProvider({
+    rpc: () => ({
+      http: "http://localhost:8545",
     }),
-    alchemyProvider({ apiKey: alchemyId, priority: 1 }),
-    infuraProvider({ apiKey: infuraId, priority: 2 }),
-    publicProvider({ priority: 0 }),
-  ],
-);
+  }),
+  alchemyProvider({ apiKey: alchemyId, priority: 1 }),
+  infuraProvider({ apiKey: infuraId, priority: 2 }),
+  publicProvider({ priority: 0 }),
+]);
 
 const { connectors } = getDefaultWallets({
   appName: "Journal of Decentalized Work",
