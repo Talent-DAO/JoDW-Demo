e973aeadee083c66986cd90f51dbb6f380bf0904
Jaxcoder <codenamejason@outlook.com>
2022-10-02 16:58:50 -0400

    add todo

--- packages/react-app/src/views/__tests__/Home.test.jsx
+++ packages/react-app/src/views/__tests__/Home.test.jsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 // import { render, screen } from "@testing-library/react";
 // import { Home } from "../Home.jsx";
 
+// todo: FIXME => test is failing due to polyfill issue
 describe("<Home /> view component", () => {
   it("should render the home view", () => {
     // render(<Home />);
