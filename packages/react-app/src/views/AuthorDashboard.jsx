aab4c6aea7bf5e5a99dd1e4aefc363927a75698c
magicpalmtree <magicpalmtree.space@gmail.com>
2022-11-07 09:10:11 -0500

    udpated sidebar

--- packages/react-app/src/views/AuthorDashboard.jsx
+++ packages/react-app/src/views/AuthorDashboard.jsx
@@ -1,32 +1,30 @@
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
-  Bars3BottomLeftIcon,
-  BellIcon,
-  CalendarIcon,
-  ChartBarIcon,
+  Bars3Icon,
   FolderIcon,
   HomeIcon,
-  InboxIcon,
-  UsersIcon,
-  XMarkIcon,
+  AdjustmentsVerticalIcon,
+  XCircleIcon,
+  PowerIcon,
 } from "@heroicons/react/24/outline";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { CustomConnectButton } from "../components";
 import { NavNotification } from "../components";
+import logoImage from "../assets/talent-logo.png";
+import discordImage from "../assets/discord-icon.png";
+import SidebarItem from "../components/dashboard/SidebarItem";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: InboxIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+  { name: "My Publification", href: "#", icon: FolderIcon, current: false },
+  { name: "Settings"  , href: "#", icon: AdjustmentsVerticalIcon, current: false },
+  { name: "Log Out", href: "#", icon: PowerIcon, current: false },
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+const DiscordImage = () => {
+  return <img src={discordImage} className="mx-2 w-4 h-3" />;
+};
 
 const AuthorDashboard = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -45,7 +43,7 @@ const AuthorDashboard = () => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+              <div className="fixed inset-0 bg-textgrey bg-opacity-75" />
             </Transition.Child>
 
             <div className="fixed inset-0 z-40 flex">
@@ -58,7 +56,7 @@ const AuthorDashboard = () => {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white pt-5 pb-4">
+                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white pt-5 pb-4 items-center rounded-r-3xl mt-12">
                   <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-300"
@@ -68,47 +66,27 @@ const AuthorDashboard = () => {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                   >
-                    <div className="absolute top-0 right-0 -mr-12 pt-2">
+                    <div className="absolute top-0 right-0 pt-4 mr-4">
                       <button
                         type="button"
                         className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         onClick={() => setSidebarOpen(false)}
                       >
                         <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        <XCircleIcon className="h-10 w-10 text-black" aria-hidden="true" />
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="flex flex-shrink-0 items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
-                    />
-                  </div>
-                  <div className="mt-5 h-0 flex-1 overflow-y-auto">
-                    <nav className="space-y-1 px-2">
-                      {navigation.map(item => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
-                            "group flex items-center px-2 py-2 text-base font-medium rounded-md",
-                          )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              item.current ? "text-gray-500" : "text-gray-400 group-hover:text-gray-500",
-                              "mr-4 flex-shrink-0 h-6 w-6",
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      ))}
+                  <div className="mt-24 h-0 flex-1 overflow-y-auto w-56">
+                    <nav className="flex-1 px-2">
+                      <div className="space-y-1">
+                        {navigation.map(item => (
+                          <SidebarItem item={item} />
+                        ))}
+                      </div>
+                      <div className="mt-48">
+                        <SidebarItem item={{ name: "Community", href: "#", icon: DiscordImage, current: true }} />
+                      </div>
                     </nav>
                   </div>
                 </Dialog.Panel>
@@ -123,63 +101,50 @@ const AuthorDashboard = () => {
         {/* Static sidebar for desktop */}
         <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5">
+          <div className="flex flex-grow flex-col overflow-y-auto border-r border-bordergrey bg-white pt-5 items-center">
             <div className="flex flex-shrink-0 items-center px-4">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              />
+              <img className="h-10 w-auto" src={logoImage} alt="TalentDAO" />
             </div>
-            <div className="mt-5 flex flex-grow flex-col">
-              <nav className="flex-1 space-y-1 px-2 pb-4">
-                {navigation.map(item => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.current ? "bg-gray-100 text-gray-900" : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
-                      "group flex items-center px-2 py-2 text-sm font-medium rounded-md",
-                    )}
-                  >
-                    <item.icon
-                      className={classNames(
-                        item.current ? "text-gray-500" : "text-gray-400 group-hover:text-gray-500",
-                        "mr-3 flex-shrink-0 h-6 w-6",
-                      )}
-                      aria-hidden="true"
-                    />
-                    {item.name}
-                  </a>
-                ))}
+            <div className="mt-24 flex flex-grow flex-col">
+              <nav className="flex-1 px-2 pb-4">
+                <div className="space-y-1">
+                  {navigation.map(item => (
+                    <SidebarItem item={item} />
+                  ))}
+                </div>
+                <div className="mt-48">
+                  <SidebarItem item={{ name: "Community", href: "#", icon: DiscordImage, current: true }} />
+                </div>
               </nav>
             </div>
           </div>
         </div>
         <div className="flex flex-1 flex-col md:pl-64">
           <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
-            <button
-              type="button"
-              className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+            <div className="flex flex-shrink-0 items-center px-4 md:hidden">
+              <img className="h-10 w-auto" src={logoImage} alt="TalentDAO" />
+            </div>
             <div className="flex flex-1 justify-end px-4">
-              <div className="ml-4 flex items-center md:ml-6 space-x-4">
+              <div className="ml-4 items-center md:ml-6 space-x-4 hidden md:flex">
                 {/* Notification dropdown */}
                 <NavNotification />
-
                 <CustomConnectButton />
               </div>
+              <button
+                type="button"
+                className="px-4 text-darkgrey focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+                onClick={() => setSidebarOpen(true)}
+              >
+                <span className="sr-only">Open sidebar</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
             </div>
           </div>
 
           <main className="flex-1">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                <h1 className="text-2xl font-semibold text-darkgray">Dashboard</h1>
               </div>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
